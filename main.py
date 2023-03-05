53fab0c522c4b041a78e85bae8682c62584e1e89
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-03-06 01:25:39 +0530

    speech2text code implementation

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello, World!"
+    transcript = ""
+    if request.method == "POST":
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audiofile = sr.AudioFile(file)
+            with audiofile as source:
+                data = recognizer.record(source)
+
+            transcript = recognizer.recognize_google(data, key=None)
+            print(transcript)
+
+    return render_template("index.html", transcript=transcript)
 
 
 if __name__ == "__main__":
